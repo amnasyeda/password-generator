3ed41f789de373991045b096d36951555379dbe8
amnasyeda <amna.syeda98@gmail.com>
2021-05-06 16:09:36 -0400

    initial-layout

--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,64 @@ var pReference = {
     }
 }
 
+// function for code 
 var generatePassword = function () {
 
+// user alert 
+  window.alert("Pick either numberd, upper/lowercase letters, or special characters.");
+
+  var validInput = true;
+
+userInput = "";
+arraySplit = "";
+exitApplixation = false;
+finalPassword = "";
+displayPassword = "";
+
+// user input selected 
+while(validInput) {
+  userInput = window.prompt("Please select the variables to be used for your password.");
+  if (!userInout) {
+    window.alert("If you would like to close the application, please close this tab.")
+    generatePassword():
+  } else if (userInput) {
+
+    userInout = userInput.split (" ");
+    arraySplit = userInput.slice(0,userInput.length);
+
+    window.alert("The follwing has been selected: " + arraySplit);
+
+    validInput = false;
+
+  //loop for user input 
+  for (var i = 0; i < userInput.length; i++) {
+
+  var inputReference = userInput [i];
+
+  var inputReferenceIntegar = parsaInt(inputReference);
+
+  switch(inputReferenceIntegar) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+
+      break;
+    default: 
+      if (!inputReferenceInteger) {
+        window.alert("Select variables.");
+        generatePassword();
+      }
+    else {
+      window.alert("Select variables.");
+      generatePassword();
+    }
+  }
 }
+  
+    
+  
+  
 
 
 // Get references to the #generate element
