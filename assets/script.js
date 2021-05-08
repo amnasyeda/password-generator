25599fb85898284caa11d8a12c18cc9ac9e055f7
amnasyeda <amna.syeda98@gmail.com>
2021-05-08 19:27:30 -0400

    password not generating in box

--- assets/script.js
+++ assets/script.js
@@ -2,21 +2,21 @@
 function generatePassword() {
 
 
-  //asking user for length of password 
+  // Asking user for length of password 
 var lengthPrompt = function() {
-  var length = prompt("How many characters will your password have? This may range from 8 to 128 characters.");
+  var length = parseInt(prompt("Please select a length for your password. This may range from 8 to 128 characters."));
 
-      // if nothing is typed, state try again
+      // If no asnwer inputted, state try again
   if (length = "", length = null, length <= 7, length >= 129) {
-      alert("Invalid answer. Try Again")
+      alert("Invalid response. Please try again")
       return lengthPrompt();
   } else {
-      // return user number input
+    
       console.log(length);
       return length;
   }
 }
-  // global variable for functions
+  // Global variables
 var length = lengthPrompt();
 
 // Uppercase variables outcome
@@ -24,11 +24,11 @@ var upperCaseF = function() {
   var charSetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var retValUpper = "";
 
-  var upperCase = window.prompt("Will you be using uppercase letters? Yes or No.");
+  var upperCase = window.prompt("Will you be using uppercase letters? State either yes or no for your response.");
 
   // If yes, randomized strings generated
   if (upperCase = "YES", upperCase = "yes", upperCase = "Yes") {
- for (var i = 0, n=charSetUpper.length; i < length; ++i) {
+ for (var i = 0, n = charSetUpper.length; i < length; ++i) {
       retValUpper += charSetUpper.charAt(Math.floor(Math.random() * n))
  }
    return retValUpper;
@@ -45,7 +45,7 @@ var lowerCaseF = function() {
   var charSetLower = "abcdefghijklmnopqrstuvqxyz";
   var retVal = "";
 
-  var lowerCase = window.prompt("Will you be using lowercase letters? Yes or No.");
+  var lowerCase = window.prompt("Will you be using lowercase letters? State either yes or no for your response.");
 
   // If yes, randomized strings generated
 if (lowerCase = "YES", lowerCase = "yes", lowerCase = "Yes") {
@@ -66,7 +66,7 @@ var numbersF = function() {
   var numberSet = "1234567890";
   var retNumber = null;
 
-  var numberPrompt = window.prompt("Will you be using numbers? Yes or No.");
+  var numberPrompt = window.prompt("Will you be using numbers? State either yes or no for your response.");
 
   // If yes, randomized strings generated
   if (numberPrompt = "YES", numberPrompt = "yes", numberPrompt = "yes") {
@@ -87,7 +87,7 @@ var numbersF = function() {
      var specialSet = "!@#$%^&*()<>?/.,;:{}[]~\|";
      var retSpecial = "";
 
-     var specialPrompt = window.prompt("Will you be using special characters? Yes or No.");
+     var specialPrompt = window.prompt("Will you be using special characters? State either yes or no for your response.");
    
      // if yes, randomized strings generated 
   if (specialPrompt = "YES", specialPrompt = "yes", specialPrompt = "Yes") {
@@ -113,6 +113,8 @@ for (var i = 0, n = alphabet.length; i < length; ++i) {
 return retValCom;
   }
 
+  var randomstring = Math.random().toString(36).slice(-8);
+   
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -127,6 +129,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
 
