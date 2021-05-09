fffe57247598dc683ac80dd624867ff760546c4e
amnasyeda <amna.syeda98@gmail.com>
2021-05-09 13:26:04 -0400

    text now displayed

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,9 @@
 let generateBtn = document.querySelector("#generate");
 console.log(generateBtn)
 
+// Array references for characters utilized
 const lc ="abcdefghijklmnopqrstuvwxyz";
 
-// Array references for characters utilized
 const uc ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const spec ="!@#$%^&*()<>?/.,;:{}[]~\|";
@@ -13,7 +13,7 @@ const num = "0123456789";
 
 let allChar = "";
 
-// Questions and answers given to user for each condition 
+// Password length input 
 function generatePassword() {
   console.log("btn was clicked")
   let pwdLength = prompt(
@@ -22,32 +22,32 @@ function generatePassword() {
   if (pwdLength < 8 || pwdLength > 128 || isNaN(parseInt(pwdLength))) {
     alert("Sorry. Your password must be more than 8 characters and less than 128 characters. Please enter a password of the required length and try again.");
   } else {
+
+ // Lowercase input 
     let lowerCase = confirm("Will you be using LOWER CASE letters in your password?");
-    if(lowerCase === true) {
-      for (var i = 0; i < lowerCase.length; i++) {
-        collectionOfValids.push(lowerCase[i]);
-    }
-}
+    if(lowerCase){
+      allChar += lc
+    };
+
+  // Uppercase input 
     let upperCase = confirm("Will you be using UPPER CASE letters in your password?");
-    if (upperCase === true) {
-      for (var i = 0; i < upperCase.length; i++) {
-        collectionOfValids.push(upperCase[i]);
-    }
-}
+    if (upperCase){
+      allChar += uc
+    };
+
+  // Special characters input 
     let specialCharacters = confirm("Will you be using SPECIAL CHARACTERS in your password?");
-    if (spec === true) {
-      for (var i = 0; i < spec.length; i++) {
-        collectionOfValids.push(spec[i]);
-    }
-}
+    if (spec){
+      allChar += spec
+    };
+
+  // Numbers input 
     let numbers = confirm("Will you be using NUMBERS in your password?");
-    if (num === true) {
-      for (var i = 0; i < num.length; i++) {
-        collectionOfValids.push(num[i]);
-    }
-}
-     
-    // console.log if no prompt was exhibited  
+    if (num){
+      allChar += num
+    };
+
+  // console.log if no prompt was exhibited  
     if (
       lowerCase === false &&
       upperCase === false &&
