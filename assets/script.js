004addfcedf37696aa0461a9e89596c7b46ef2b7
amnasyeda <amna.syeda98@gmail.com>
2021-05-09 15:07:49 -0400

    remove unneccesary text

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let generateBtn = document.querySelector("#generate");
 console.log(generateBtn)
@@ -80,44 +79,3 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 console.log(generateBtn)
 
-
-=======
-// Assignment code here
-var userInput = "";
-var arraySplit = "";
-var exitApplixation = false;
-var finalPassword = "";
-var displayPassword = "";
-
-// reference for randomized strings
-var pReference = {
-    numbers: { indexNumber: 0, reference: "0123456789"
-    },
-    uppercase: { indexNumber: 1, reference: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"        
-    }, 
-    lowercase: { indexNumber: 2, reference: "abcdefghijklmnopqrstuvwxyz"
-    },
-    characters: { indexNumber: 3, reference: "!@#$%^&*()<>?/.,;:{}[]~\|"
-    }
-}
-
-var generatePassword = function () {
-
-}
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
->>>>>>> main
